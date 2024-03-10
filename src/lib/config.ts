3dbabb0d475d4f75801d1548867fd1996c0016f8
Vinlic <vinlic@vinlic.com>
2024-03-10 15:36:26 +0800

    移除配置

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,5 @@
 import serviceConfig from "./configs/service-config.ts";
 import systemConfig from "./configs/system-config.ts";
-import apiConfig from "./configs/api-config.ts";
 
 class Config {
     
@@ -9,9 +8,6 @@ class Config {
     
     /** 系统配置 */
     system = systemConfig;
-
-    /** API配置 */
-    api = apiConfig;
 
 }
 
